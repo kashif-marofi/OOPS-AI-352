5b23fa11761ba7a11395d17fd50e4f7d4ad410b5
Marofi3939 <your_email@github.com>
2026-08-11 13:25:31 +0500

    Week 05 Class Work uploaded

---
+++ week_05/classWork.py
@@ -0,0 +1,87 @@
+# Area and Circumfrance of Circle 
+
+# class Circle :
+#     def __init__ (self, rad, color,):
+#         self.rad = rad
+#         self.color = color
+
+#     def calculate_Area(self):
+#         area = 3.142 * self.rad ** 2
+#         print(f"Area of Circle:  {area}")
+#     def calculate_Circum(self):
+#         circum = 2 * 3.142 * self.rad
+#         print(f"Area of Circle:  {circum}")
+
+# c1 = Circle(int(input("Radius: ")), input("color: "))
+# c1.calculate_Area()
+# c1.calculate_Circum()
+
+
+# Multiple defs of __init:
+
+# class Square:
+#     def __init__(self, s, l, c, f):
+#         self.__side = s
+#         self.len = l
+#         self.col = c
+#         self.filled = f
+
+#     def __init__(self, s, l, c, f = False):       # This latest fn() always run
+#             self.__side = s
+#             self.len = l
+#             self.col = c
+#             self.filled = f
+#     def info(self):
+#          print(f"This shape has {self.__side} sides, and has length {self.len}, and its color is {self.col}, and FIlled {self.filled}")        
+
+# obj1 = Square(4, 5, "red", True)
+# obj2 = Square(4, 5, "red")
+# obj1.info()
+# obj2.info()
+
+# Function Overloading:
+
+# def fn(a, b):
+#     return a + b
+# def fn(a, b, c):        # As there is no fn( overloading in Python by default so the latest fn() will run
+#     return a + b + c
+
+# # fn(1,2)       # Error: 3 arguments are required???
+# print(fn(1,2,3))
+
+# List of Shapes:
+
+class Shapes:
+    def __init__(self, s, l, c, f = False):
+        self.__side = s
+        self.len = l
+        self.col = c
+        self.filled = f
+
+    def show_Info(self,):
+        if self.__side < 3 or self.__side > 8:
+            print("Invalid input")
+        elif self.__side == 3:
+            print(f"It is a Triangle, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+        elif self.__side == 4:
+            print(f"It is a Square, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+        elif self.__side == 5:
+            print(f"It is a Pentagon, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+        elif self.__side == 6:
+            print(f"It is a Hexagon, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+        elif self.__side == 7:
+            print(f"It is a Heptagon, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+        elif self.__side == 8:
+            print(f"It is a Octagon, has side: {self.__side} , and length is {self.len}, of color: {self.col}")
+
+o_l = []
+n = int(input("Enter a number: "))
+for i in range(n):
+    side = int(input(f"Sides of shape {i + 1}: "))
+    length = int(input(f"Length of shape {i + 1}: "))
+    color = input(f"Color of shape {i + 1}: ")
+    filled = input(f"FIlled of shape {i + 1}:")
+    s = Shapes(side, length, color, filled)
+    o_l.append(s)
+for i in o_l:
+    i.show_Info()  
